97414c82ec968e5efcee70b1011278923005c050
gitGuedes <joaofcguedes1990@hotmail.com>
2025-10-26 22:56:27 +0000

    Updates

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-
-const supabaseUrl = 'https://eepwzjikyqscklrcuvhd.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlcHd6amlreXFzY2tscmN1dmhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MDAzMTUsImV4cCI6MjA3NjA3NjMxNX0.QGAqkY2ttUKNKAQHp6thTpH58Jy-TvowawzhoLJkHQc'; // Substitui pela tua public key
-export const supabase = createClient(supabaseUrl, supabaseKey);
